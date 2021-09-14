51c35b6fc67f3e98aadf8027570b0cdb6618e0e2
Ubuntu <ubuntu@ip-172-31-81-184.ec2.internal>
2021-09-14 16:43:53 +0000

    added Tau program.

---
+++ constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Created by: Mikayla Barthelette
+# Created on: Sept 2021
+# This program creates a constant for tau
+
+import math
+
+TAU = 2 * math.pi
---
+++ tau.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Mikayla Barthelette
+# Created on: Sept 2021
+# This program finds the circumference of a circle using tau
+
+import math
+import constants
+
+
+def main():
+    # this function calculates the circumference
+
+    # input
+    radius = int(input("Enter the radius of the circle (mm): "))
+
+    # process
+    circumference = constants.TAU * radius
+
+    # output
+    print("The circumference is: {} mm.".format(circumference))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
